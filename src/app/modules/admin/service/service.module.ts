98d7c1f8d85d730f9db16d882b984306974baae1
Franco <frcg2424@hotmail.com>
2022-05-06 14:38:49 -0500

    feature: add register customer logical

--- src/app/modules/admin/service/service.module.ts
+++ src/app/modules/admin/service/service.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {ServiceComponent} from './service.component';
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import {CommonModule} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 const exampleRoutes: Route[] = [
     {
@@ -30,7 +32,9 @@ const exampleRoutes: Route[] = [
         MatListModule,
         MatIconModule,
         CommonModule,
-        FormsModule
+        FormsModule,
+        MatInputModule,
+        MatSelectModule
     ]
 })
 export class ServiceModule
